667a00e127fdddb77804c91dbed51cf52802b174
itarek99 <tarekulislam7060@gmail.com>
2023-05-04 12:52:45 +0600

    get and post req added

--- controllers/tools.controller.js
+++ controllers/tools.controller.js
@@ -1,12 +1,22 @@
+const tools = [
+  { id: 1, title: "Hammer" },
+  { id: 2, title: "Screwdriver" },
+  { id: 3, title: "Pliers" },
+  { id: 4, title: "Wrench" },
+  { id: 5, title: "Chisel" },
+];
+
 const getAllTools = (req, res) => {
-  res.send("tools found");
+  res.json(tools);
 };
 const addNewTool = (req, res) => {
-  res.send("tool add");
+  const newTool = req.body;
+  tools.push(newTool);
+  res.json(tools);
 };
 const toolDetails = (req, res) => {
   const { params } = req;
-
-  res.send("tool details for " + params.id);
+  const tool = tools.find((tool) => tool.id === +params.id);
+  res.json(tool);
 };
 module.exports = { getAllTools, addNewTool, toolDetails };
